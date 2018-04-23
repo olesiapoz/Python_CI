4a4c47493970e65bec952c98ea130e8f3858579d
ms <--->
2018-04-23 12:47:27 +0300

    Travice CI

--- app-test.py
+++ app-test.py
@@ -5,6 +5,7 @@ class MyUnitTest(unittest.TestCase):
     def test_my_function(self):
         self.assertEqual(app.my_function(1,1),2)
         self.assertEqual(app.my_function(1,2.2),3.2)
+        self.assertEqual(app.my_function(1,5.2),6.2)
 
 if __name__ == '__main__' :
     unittest.main()
